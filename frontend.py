95f40252f989fa2ce26193a84510c9d5552fae04
zacherylzy <88877428+zacherylzy@users.noreply.github.com>
2024-01-24 19:29:00 +0800

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -2,3 +2,45 @@ import streamlit as st
 st.title('Toastmasters Table Topics Generator')
 
 streamlit run https://gist.githubusercontent.com/zacherylzy/5849f58084ec876c854f4a1b4ba692d1/raw/0c2d519a1a787a58caab4125e7fd2fec831f7775/backend.py
+
+
+
+
+from random import randint
+
+alltopics = []
+completedtopics = []
+
+
+#read all the topics, add them to a temporary list
+topics = open("topics.txt", "r")
+for line in topics:
+    alltopics.append(line.strip())
+
+#read the completed file first
+completed = open("completed.txt", "r")
+for lines in completed:
+    completedtopics.append(lines.strip())
+
+#write to completed
+addcompleted = open("completed.txt", "a")
+
+
+for i in range(len(alltopics)):
+    i = randint(0, len(alltopics))
+    print(i)
+    if alltopics[i] not in completedtopics:  #not yet completed/found in completed so break and print the i
+        print(alltopics[i])
+        addcompleted.write(alltopics[i])
+        addcompleted.write("\n")
+        addcompleted.close()
+        break
+    else: #already completed/found in completed, continue loop again
+        continue
+
+
+#for i in alltopics:
+    #if i[-1] != "?":
+       # print(i)
+
+#add a random factor inside
