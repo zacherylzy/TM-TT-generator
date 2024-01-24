c133a8cba295a574f2cd07d36b820e2517ff5c12
zacherylzy <88877428+zacherylzy@users.noreply.github.com>
2024-01-24 19:22:36 +0800

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,3 +1,4 @@
 import streamlit as st
 st.title('Toastmasters Table Topics Generator')
 
+streamlit run https://gist.githubusercontent.com/zacherylzy/5849f58084ec876c854f4a1b4ba692d1/raw/0c2d519a1a787a58caab4125e7fd2fec831f7775/backend.py
